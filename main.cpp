8ef8745cae7b4207e6d3f7e7e3cd4f161a19d0bb
Brave Mugisha <bravemugisha@gmail.com>
2024-02-16 15:49:19 -0500

    added printcoords; testing randCoords

--- main.cpp
+++ main.cpp
@@ -5,25 +5,40 @@
 using namespace std;
 
 double Prims(vector<vector<double>> RC); 
-double avg(vector<vector<double>> RC, int n, int d); 
+void printCoords(const vector<vector<double>>& coords);
+double avg(int n, int d); 
 
 int main() {
-    
+    avg(10, 2);
+    return 0;
 }
 
 // Function to calculate the average weight of the MST
 // input: list of tuples of coordinates, number of vertices, number of dimensions
 // output: average weight of the MST
-double avg(vector<vector<double>> RC, int n, int d) {
+double avg(int n, int d) {
     double sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += Prims(randomCoords(n, d));
-    }
-    return sum / n;
+    // for (int i = 0; i < n; i++) {
+        sum = Prims(randomCoords(n, d));
+    // }
+    printCoords(randomCoords(n, d));
+    std::cout << "The output of Prim is: " << sum << std::endl;
     std::cout << "The average weight of the MST is: " << sum / n << std::endl;
+    return sum / n;
 }
 
 
+// Function to print the coordinates
+void printCoords(const vector<vector<double>>& coords) {
+    for (const auto &vec : coords) {
+        cout << "(";
+        for (size_t i = 0; i < vec.size(); ++i) {
+            cout << vec[i];
+            if (i < vec.size() - 1) cout << ", ";
+        }
+        cout << ")" << endl;
+    }
+}
 
 
 // To do's: 
