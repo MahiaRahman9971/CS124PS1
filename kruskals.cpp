abc08888cb20596298f70d2f19d29d6d7bfc93b9
mahiarahman@college.harvard.edu <90074806+MahiaRahman9971@users.noreply.github.com>
2024-02-19 22:02:43 -0500

    new kruskals

--- kruskals.cpp
+++ kruskals.cpp
@@ -9,77 +9,71 @@ using namespace std;
 // Disjoint set data structure
 class DisjointSet {
 private:
-    vector<int> parent, rank;
+   vector<int> parent, rank;
 
 public:
-    DisjointSet(int n) : parent(n), rank(n, 0) {
-        for (int i = 0; i < n; ++i) {
-            parent[i] = i;
-        }
-    }
+   DisjointSet(int n) : parent(n), rank(n, 0) {
+       for (int i = 0; i < n; ++i) {
+           parent[i] = i;
+       }
+   }
 
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);  // Path compression
-        }
-        return parent[x];
-    }
+   int find(int x) {
+       if (x != parent[x]) {
+           parent[x] = find(parent[x]);  // Path compression
+       }
+       return parent[x];
+   }
 
-    void unite(int x, int y) {
-        int xRoot = find(x);
-        int yRoot = find(y);
-        if (xRoot == yRoot) return;
+   void unite(int x, int y) {
+       link(find(x), find(y));
+   }
 
-        // Union by rank
-        if (rank[xRoot] < rank[yRoot]) {
-            parent[xRoot] = yRoot;
-        } else if (rank[xRoot] > rank[yRoot]) {
-            parent[yRoot] = xRoot;
-        } else {
-            parent[yRoot] = xRoot;
-            rank[xRoot] += 1;
-        }
-    }
+   int link(int x, int y) {
+       if (rank[x] > rank[y]) {
+           return link(y, x);
+       } else {
+           if (rank[x] == rank[y]) {
+               rank[y]++;
+           }
+       }
+       parent[x] = y;
+       return y;
+   };
 };
 
 // Kruskal's algorithm to find MST
 // input: number of vertices, graph
 // output: weight of the MST
 float kruskalMST(int n, vector<pair<pair<int, int>, float>>& graph) {
-    sort(graph.begin(), graph.end(), [](const tuple<tuple<int, int>, float>& a, const tuple<tuple<int, int>, float>& b) {
-        return get<1>(a) < get<1>(b);
-    });
+   sort(graph.begin(), graph.end(), [](const pair<pair<int, int>, float>& a, const pair<pair<int, int>, float>& b) {
+       return a.second < b.second;
+   });
 
-    DisjointSet ds(n);
-    vector<pair<pair<int, int>, float>> mst;
-
-    for (const auto& edge : graph) {
-        int u = get<0>(get<0>(edge));
-        int v = get<1>(get<0>(edge));
-
-        if (ds.find(u) != ds.find(v)) {
-            mst.push_back(edge);
-            ds.unite(u, v);
-        }
-    }
-
-    float mstWeight = 0;
-    for (const auto& edge : mst) {
-        mstWeight += get<1>(edge);
-    }
-    return mstWeight;
+   DisjointSet ds(n);
+   float MSTweight = 0.0;
+   for (const auto& edge : graph) {
+       int u = edge.first.first;
+       int v = edge.first.second;
+       float w = edge.second;
+       if (ds.find(u) != ds.find(v)) {
+           MSTweight += w;
+           ds.unite(u, v);
+       }
+   }
+   return MSTweight;
 }
 
-int main() {
-    int n = 5;  // Number of vertices
-    int d = 2;  // Number of dimensions
-    // vector<vector<float>> RC = randomCoords(n, d);
-    vector<vector<float>> RC = {{0.4603, 0.44525},{0.591422, 0.80881}, {0.516874, 0.039684}, {0.768217, 0.660744}};
-    vector<pair<pair<int, int>, float>> graph = generateGraph(RC);
+// int main() {
+//     int n = 5;  // Number of vertices
+//     int d = 2;  // Number of dimensions
+//     // vector<vector<float>> RC = randomCoords(n, d);
+//     vector<vector<float>> RC = {{0.4603, 0.44525},{0.591422, 0.80881}, {0.516874, 0.039684}, {0.768217, 0.660744}};
+//     vector<pair<pair<int, int>, float>> graph = generateGraph(RC);
 
-    // Apply Kruskal's algorithm
-    float weight = kruskalMST(n, graph);
-    cout << weight << endl;
+//     // Apply Kruskal's algorithm
+//     float weight = kruskalMST(n, graph);
+//     cout << weight << endl;
 
-    return 0;
-}
+//     return 0;
+// }
